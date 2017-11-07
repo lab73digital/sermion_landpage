8c5aca416bf1af48c5568f0d33289f637a445dc4
chesovnik <kitkatne@gmail.com>
2017-11-07 13:33:23 +0200

    added favicon, fixed puzzle-graph

--- app/js/common.js
+++ app/js/common.js
@@ -141,188 +141,190 @@ $(document).ready(function () {
 
 
     $('.to-sermion').on('click', function () {
-        if ((!toSermionClicked) && scrollHeadAnimFinished) {
-            toSermionClicked = true;
-            headPuzzleGatherAll();
-            if (!disclaimerShowed) {
-                $("body").css("overflow", "hidden");
-                $(".disclaimer_overlay").css("visibility", "visible");
-                new TimelineMax().set('.disclaimer', {
-                    display: 'block'
-                }).fromTo('.disclaimer', .7, {
-                    opacity: 0
-                }, {
-                    opacity: 1
-                });
-                $('.head-svg').css('pointer-events', 'none');
-                //console.log('pointer-events:none');
-                //$('.disclaimer').show('slow');
-            } else {
-                $('.parallax-mirror').css('display', 'block');
-            }
-
-            $('.head-svg').css('width', 'auto');
-            scrolledToBottom = false;
-
-            new TimelineMax().to('.screen-page:visible', 0.5, {
-                opacity: 0,
-                display: 'none',
-                ease: Power1.easeInOut,
-                onComplete: function () {
-                    $(window).scrollTop(0);
-                }
-            }).fromTo('.sermion-page', 0.5, {
-                opacity: 0,
-                display: 'none',
-                y: $(window).height(),
-                ease: Power1.easeInOut
-            }, {
-                opacity: 1,
-                display: 'block',
-                y: 0,
-                ease: Power1.easeInOut,
-                onComplete: function () {
-                    //$('.parallax-mirror').css('display','block');
-                    // $('.parallax').parallax({
-                    //     imageSrc: 'img/doc-parallax/inside-bg.jpg',
-                    //     speed: 0.8
-                    // });
-                    toSermionClicked = false;
-                    var tween0 = new TimelineMax().staggerFrom('.parallax_text', 0.9,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0,
-                            y: 50
-                        }, 0.3);
-                    var tween5 = new TimelineMax().from('.animation_sermion-page_1', 0.6,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        }).from('.animation_sermion-page_2', 0.5,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        }).from('.animation_sermion-page_3', 0.5,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        }).from('.animation_sermion-page_4', 0.5,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        }).from('.animation_sermion-page_5', 0.5,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        }).from('.animation_sermion-page_6', 0.5,
-                        {
-                            ease: Power1.easeInOut,
-                            opacity: 0
-                        });
-                    var tween1 = new TimelineMax().fromTo('.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent', 1, {
+        if (scrollHeadAnimFinished) {
+            if (!toSermionClicked) {
+                toSermionClicked = true;
+                headPuzzleGatherAll();
+                if (!disclaimerShowed) {
+                    $("body").css("overflow", "hidden");
+                    $(".disclaimer_overlay").css("visibility", "visible");
+                    new TimelineMax().set('.disclaimer', {
+                        display: 'block'
+                    }).fromTo('.disclaimer', .7, {
                         opacity: 0
                     }, {
                         opacity: 1
-                    }).add(TweenMax.from('.s-3__graph-circle, .s-3__graph-percent', 1, {
-                        bottom: 0
-                    }), '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent').add(TweenMax.from('.s-3__graph-line', 1, {
-                        height: 0
-                    }), '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent').add(function () {
-                        $('.s-3__animate-number').each(function () {
-                            var number = $(this).data('number');
-                            var decimal_places = 1;
-                            var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
-                            $(this).animateNumber({
-                                number: number * decimal_factor,
-
-                                numberStep: function (now, tween) {
-                                    var floored_number = Math.floor(now) / decimal_factor,
-                                        target = $(tween.elem);
-
-                                    if (decimal_places > 0) {
-                                        // force decimal places even if they are 0
-                                        floored_number = floored_number.toFixed(decimal_places);
-
-                                        // replace '.' separator with ','
-                                        floored_number = floored_number.toString();
-                                    }
-
-                                    target.text(floored_number);
-                                }
-                            }, 1000)
-                        });
-                    }, '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent');
-
-                    var tween2 = new TimelineMax().from('.s-4__graph-item_row--1', .6, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        y: -50
-                    }).from('.s-4__graph-item_row--2', .6, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        y: -50
-                    }).from('.s-4__graph-item_row--3', .6, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        y: -50
-                    }).from('.s-4__graph-item_row--4', .6, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        y: -50
-                    }).from('.s-4__graph-item_row--5', .6, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        y: -50
                     });
-
-                    var tween3 = new TimelineMax().staggerFrom('.td-circle_animation--1', .9, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        width: 0,
-                        height: 0
-                    }, 0.3).staggerFrom('.td-circle_animation--2', .9, {
-                        ease: Power1.easeInOut,
-                        opacity: 0,
-                        width: 0,
-                        height: 0
-                    }, 0.3);
-                    var tween4 = new TimelineMax().staggerTo('.td-circle_animation--1 p', .9, {
-                        ease: Power1.easeInOut,
-                        opacity: 1
-                    }, 0.3).staggerTo('.td-circle_animation--2 p', .9, {
-                        ease: Power1.easeInOut,
-                        opacity: 1
-                    }, 0.3);
-
-                    new ScrollMagic.Scene({
-                        triggerElement: ".s-3",
-                        reverse: false
-                    }).setTween(tween1).addTo(controller);
-                    new ScrollMagic.Scene({
-                        triggerElement: ".s-4",
-                        reverse: false
-                    }).setTween(tween2).addTo(controller);
-                    new ScrollMagic.Scene({
-                        triggerElement: ".s-5",
-                        reverse: false
-                    }).setTween(tween3).addTo(controller);
-                    new ScrollMagic.Scene({
-                        triggerElement: ".s-5",
-                        reverse: false
-                    }).setTween(tween4).addTo(controller);
-                    new ScrollMagic.Scene({
-                        triggerElement: ".parallax",
-                        reverse: false
-                    }).setTween(tween0).addTo(controller);
-                    new ScrollMagic.Scene({
-                        triggerElement: ".animation_sermion-page_1",
-                        triggerHook: 'onEnter',
-                        offset: 200,
-                        reverse: false
-                    }).setTween(tween5).addTo(controller);
-
+                    $('.head-svg').css('pointer-events', 'none');
+                    //console.log('pointer-events:none');
+                    //$('.disclaimer').show('slow');
+                } else {
+                    $('.parallax-mirror').css('display', 'block');
                 }
-            });
+
+                $('.head-svg').css('width', 'auto');
+                scrolledToBottom = false;
+
+                new TimelineMax().to('.screen-page:visible', 0.5, {
+                    opacity: 0,
+                    display: 'none',
+                    ease: Power1.easeInOut,
+                    onComplete: function () {
+                        $(window).scrollTop(0);
+                    }
+                }).fromTo('.sermion-page', 0.5, {
+                    opacity: 0,
+                    display: 'none',
+                    y: $(window).height(),
+                    ease: Power1.easeInOut
+                }, {
+                    opacity: 1,
+                    display: 'block',
+                    y: 0,
+                    ease: Power1.easeInOut,
+                    onComplete: function () {
+                        //$('.parallax-mirror').css('display','block');
+                        // $('.parallax').parallax({
+                        //     imageSrc: 'img/doc-parallax/inside-bg.jpg',
+                        //     speed: 0.8
+                        // });
+                        toSermionClicked = false;
+                        var tween0 = new TimelineMax().staggerFrom('.parallax_text', 0.9,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0,
+                                y: 50
+                            }, 0.3);
+                        var tween5 = new TimelineMax().from('.animation_sermion-page_1', 0.6,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            }).from('.animation_sermion-page_2', 0.5,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            }).from('.animation_sermion-page_3', 0.5,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            }).from('.animation_sermion-page_4', 0.5,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            }).from('.animation_sermion-page_5', 0.5,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            }).from('.animation_sermion-page_6', 0.5,
+                            {
+                                ease: Power1.easeInOut,
+                                opacity: 0
+                            });
+                        var tween1 = new TimelineMax().fromTo('.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent', 1, {
+                            opacity: 0
+                        }, {
+                            opacity: 1
+                        }).add(TweenMax.from('.s-3__graph-circle, .s-3__graph-percent', 1, {
+                            bottom: 0
+                        }), '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent').add(TweenMax.from('.s-3__graph-line', 1, {
+                            height: 0
+                        }), '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent').add(function () {
+                            $('.s-3__animate-number').each(function () {
+                                var number = $(this).data('number');
+                                var decimal_places = 1;
+                                var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+                                $(this).animateNumber({
+                                    number: number * decimal_factor,
+
+                                    numberStep: function (now, tween) {
+                                        var floored_number = Math.floor(now) / decimal_factor,
+                                            target = $(tween.elem);
+
+                                        if (decimal_places > 0) {
+                                            // force decimal places even if they are 0
+                                            floored_number = floored_number.toFixed(decimal_places);
+
+                                            // replace '.' separator with ','
+                                            floored_number = floored_number.toString();
+                                        }
+
+                                        target.text(floored_number);
+                                    }
+                                }, 1000)
+                            });
+                        }, '.s-3__graph-line, .s-3__graph-circle, .s-3__graph-percent');
+
+                        var tween2 = new TimelineMax().from('.s-4__graph-item_row--1', .6, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            y: -50
+                        }).from('.s-4__graph-item_row--2', .6, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            y: -50
+                        }).from('.s-4__graph-item_row--3', .6, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            y: -50
+                        }).from('.s-4__graph-item_row--4', .6, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            y: -50
+                        }).from('.s-4__graph-item_row--5', .6, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            y: -50
+                        });
+
+                        var tween3 = new TimelineMax().staggerFrom('.td-circle_animation--1', .9, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            width: 0,
+                            height: 0
+                        }, 0.3).staggerFrom('.td-circle_animation--2', .9, {
+                            ease: Power1.easeInOut,
+                            opacity: 0,
+                            width: 0,
+                            height: 0
+                        }, 0.3);
+                        var tween4 = new TimelineMax().staggerTo('.td-circle_animation--1 p', .9, {
+                            ease: Power1.easeInOut,
+                            opacity: 1
+                        }, 0.3).staggerTo('.td-circle_animation--2 p', .9, {
+                            ease: Power1.easeInOut,
+                            opacity: 1
+                        }, 0.3);
+
+                        new ScrollMagic.Scene({
+                            triggerElement: ".s-3",
+                            reverse: false
+                        }).setTween(tween1).addTo(controller);
+                        new ScrollMagic.Scene({
+                            triggerElement: ".s-4",
+                            reverse: false
+                        }).setTween(tween2).addTo(controller);
+                        new ScrollMagic.Scene({
+                            triggerElement: ".s-5",
+                            reverse: false
+                        }).setTween(tween3).addTo(controller);
+                        new ScrollMagic.Scene({
+                            triggerElement: ".s-5",
+                            reverse: false
+                        }).setTween(tween4).addTo(controller);
+                        new ScrollMagic.Scene({
+                            triggerElement: ".parallax",
+                            reverse: false
+                        }).setTween(tween0).addTo(controller);
+                        new ScrollMagic.Scene({
+                            triggerElement: ".animation_sermion-page_1",
+                            triggerHook: 'onEnter',
+                            offset: 200,
+                            reverse: false
+                        }).setTween(tween5).addTo(controller);
+
+                    }
+                });
+            }
         }
     });
 
